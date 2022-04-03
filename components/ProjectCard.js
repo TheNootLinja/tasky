e9f316e8e9ba37f0396fa5a02c4790475cd497f6
TheNootLinja <thenootlinja@gmail.com>
2022-04-03 02:01:07 -0400

    Add code to ProjectCard component to display information from the project passed in as props

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,9 +1,20 @@
-const ProjectCard = () => {
+import styled from 'styled-components'
+
+const ProjectCard = ({name, owner}) => {
     return ( 
-        <div>
-            
-        </div>
+        <ProjectCardContainer>
+            <p>{name}</p>
+            <p>{owner}</p>
+        </ProjectCardContainer>
      );
 }
  
 export default ProjectCard;
+
+const ProjectCardContainer = styled.div`
+    height: 50px;
+    width: 358px;
+    margin: auto;
+    border-radius: 10px;
+    box-shadow: 0 4px 20px rgba(0,0,0,.30)
+`;
