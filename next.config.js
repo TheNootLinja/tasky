2dad9b488efce51e890927e81b44dd41681e161c
TheNootLinja <thenootlinja@gmail.com>
2022-04-03 19:58:32 -0400

    Add styled-components to the next.config.js file so it is using SWC and remove .babelrc and _document.js as they are no longer needed with this method.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
+  compiler: {
+    styledComponents: true,
   }
 }
 
