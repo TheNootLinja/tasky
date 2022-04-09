23d72cb3248989e17e474afff7f9369c0e3918d1
TheNootLinja <thenootlinja@gmail.com>
2022-04-09 14:19:28 -0400

    Modify projects page to fetch the list of projects from that database instead of just using a hard coded array of items

--- pages/projects.js
+++ pages/projects.js
@@ -1,35 +1,43 @@
 import styled from 'styled-components'
+import { useState } from 'react'
+
+import { connectToDatabase } from '../lib/mongodb';
 
 import PrimaryButton from "../components/PrimaryButton";
 import ProjectCard from '../components/ProjectCard';
+import NewProjectForm from '../components/NewProjectForm';
 
-const projects = () => {
+const projects = ({projects}) => {
 
-    const projectsList = [
-        {
-            name: 'project #1',
-            number: 'abd123',
-            owner: 'Nicholas Peters',
-        },
-        {
-            name: 'project #2',
-            number: 'def456',
-            owner: 'Nicholas Peters',
-        },
-        {
-            name: 'project #3',
-            number: 'ghi789',
-            owner: 'Nicholas Peters',
-        },
-    ]
+    const defaultFormState = {
+        projectName: '',
+        projectDescription: '',
+    }
+
+    const [showForm, setShowForm] = useState(false);
+    const [projectFormState, setProjectFormState] = useState(defaultFormState);
+
+    const handleFormState = (e, type) => {
+        setProjectFormState({
+            ...projectFormState,
+            [type]: e.target.value
+        })
+    }
+
+    const handleFormVisible = () => {
+        setShowForm(!showForm);
+    }
+
+
+    const projectsList = projects;
 
     return ( 
         <PageContainer>
             <h1>Projects</h1>
-            <PrimaryButton buttonMargin='0 auto 20px auto'>Add Project +</PrimaryButton>
+            {showForm ? <NewProjectForm closeForm={handleFormVisible}/> : <PrimaryButton clickFunc={handleFormVisible} buttonMargin='0 auto 20px auto'>Add Project +</PrimaryButton>}
             <ProjectCardContainer>
                 {projectsList.map((project) => {
-                    return <ProjectCard key={project.number} {...project}/>
+                    return <ProjectCard key={project._id} {...project}/>
                 })}
             </ProjectCardContainer>
         </PageContainer>
@@ -37,6 +45,15 @@ const projects = () => {
 }
 
 export default projects;
+
+export async function getServerSideProps(context){
+    const { db } = await connectToDatabase();
+    const data = await db.collection('projects').find({}).toArray();
+    const projects = JSON.parse(JSON.stringify(data));
+     return {
+         props: { projects }
+     }
+  }
 
 const PageContainer = styled.div`
     width: 100%;
