45bcdb6b842c0db6799639be56ae258cf8d50dc0
TheNootLinja <thenootlinja@gmail.com>
2022-04-15 16:09:16 -0400

    Modify TaskCard component so that box shadow doesn't overflow onto neighboring elements

--- components/TaskCard.js
+++ components/TaskCard.js
@@ -1,9 +1,45 @@
+import styled from 'styled-components'
+
 const TaskCard = ({taskName}) => {
     return ( 
-        <div>
-            <p>{taskName}</p>
-        </div>
+        <TaskCardContainer>
+            <TaskName>{taskName}</TaskName>
+            <DeleteIcon onClick={() => alert('Deleting Item')}>X</DeleteIcon>
+        </TaskCardContainer>
      );
 }
  
 export default TaskCard;
+
+const TaskCardContainer = styled.div`
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    margin: auto;
+    float: left;
+    width: 90%;
+    height: 150px;
+    max-width: 450px;
+    background: #fff;  
+    position: relative;
+    padding: 20px;
+    &:before {
+        border-radius: inherit;
+        content: "";
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: -1;
+        box-shadow: 0 4px 20px rgba(0,0,0,.30);
+    };
+`;
+
+const TaskName = styled.p`
+    font-size: 1.5rem;
+`;
+
+const DeleteIcon = styled.p`
+    color: red;
+`;
