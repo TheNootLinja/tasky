fc5859d52a952120d7cd6039afcba34179592ac1
TheNootLinja <thenootlinja@gmail.com>
2022-04-19 22:09:24 -0400

    Modify ProjectCard so that when hovered over, a button menu becomes visible with links to tasks and project settings

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,25 +1,30 @@
 import styled from 'styled-components'
+import Link from 'next/link';
 
 const ProjectCard = (project) => {
     return ( 
         <ProjectCardContainer>
+            
             <InfoContainer>
                 <div>
                     <ProjectName>{project.projectName}</ProjectName>
                 </div>
             </InfoContainer>
-            <TestOpenButton>
-                <OpenTaskText  color="red">3 Urgent Priority</OpenTaskText>
+            <ButtonMenu>
+                {/* <OpenTaskText  color="red">3 Urgent Priority</OpenTaskText>
                 <OpenTaskText  color="orange">10 High Priority</OpenTaskText>
-                <OpenTaskText  color="green">25 Low Priority</OpenTaskText>
-            </TestOpenButton>
+                <OpenTaskText  color="green">25 Low Priority</OpenTaskText> */}
+                <StyledLink href='/tasks'>Tasks</StyledLink>
+                <StyledDivider/>
+                <StyledLink href='/settings'>Project Settings</StyledLink>
+            </ButtonMenu>
         </ProjectCardContainer>
      );
 }
  
 export default ProjectCard;
 
-const TestOpenButton = styled.div`
+const ButtonMenu = styled.div`
     background-color: #ccc;
     height: 100%;
     width: 0;
@@ -31,6 +36,13 @@ const TestOpenButton = styled.div`
     justify-content: space-evenly;
     align-items: center;
     overflow: hidden;
+    
+`;
+
+const StyledDivider = styled.div`
+    height: 1px;
+    width: 95%;
+    background: rgba(0,0,0,0.2);
 `;
 
 const ProjectCardContainer = styled.div`
@@ -46,13 +58,23 @@ const ProjectCardContainer = styled.div`
     transition: transform .5s;
     transition: box-shadow .5s;
     &:hover{
+        /* transform: scaleY(1.2); */
         transform: scale(1.1);
         box-shadow: 0 7px 30px rgba(0,0,0,.2);
-        ${TestOpenButton}{
+        ${ButtonMenu}{
             width: 150px;
             opacity: 100;
         }
     }
+`;
+
+const StyledLink = styled.a`
+  display: inline-block;
+  padding: 0.5rem;
+  /* margin: 0.5rem 1rem 0.5rem 2rem; */
+  color: #5E3CF5;
+  font-weight: 600;
+  white-space: nowrap;
 `;
 
 const InfoContainer = styled.div`
