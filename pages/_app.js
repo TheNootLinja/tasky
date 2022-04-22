036c5cf21434a1fe124e23939456a816c7e6726f
TheNootLinja <thenootlinja@gmail.com>
2022-04-22 14:31:20 -0400

    Modify _app.js to wrap application in ContextProvider for global state

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
+import { ContextProvider } from '../context/globalState';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <ContextProvider>
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </SessionProvider>
+    </ContextProvider>
   );
 }
 
